ca417f37bc0ee924df4ddac6e662e3e8eb2db5a8
Abhinav1281 <abhisione2@gmail.com>
2020-11-04 01:45:37 +0530

    4th lab files changed

--- 4th lab/greatest.cpp
+++ 4th lab/greatest.cpp
@@ -1,15 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-class great
+class great2;
+class great1
 {
     int num;
     public:
     void get(int x){
         num=x;
     }
-    friend void gcalc(great,great);
+    friend void gcalc(great1,great2);
 };
-void gcalc(great a,great b)
+class great2
+{
+    int num;
+    public:
+    void get(int x){
+        num=x;
+    }
+    friend void gcalc(great1,great2);
+};
+void gcalc(great1 a,great2 b)
 {
     //cout<<a.num<<" "<<b.num;
     if(a.num>b.num)
@@ -22,13 +32,13 @@ int main()
     int a,b;
     cout<<"ENTER 1ST NUMBER:";
     cin>>a;
-    great ab;
+    great1 ab;
     ab.get(a);
     //cout<<a;
     cout<<"ENTER 2ND NUMBER:";
     cin>>b;
    // cout<<a;
-    great bb;
+    great2 bb;
     bb.get(b);
     gcalc(ab,bb);
     
