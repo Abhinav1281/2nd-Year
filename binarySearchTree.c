0894f04e6f850ef9ed9f3271380ca7c610a7f2b9
Abhinav1281 <abhisione2@gmail.com>
2020-11-06 12:12:37 +0530

    Directory Structure Changed

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -71,10 +71,89 @@ void search(int searchData,node current)
     
 }
 
+node minNode(node current)
+{
+    node lsmallest=NULL,rsmallest=NULL;
+   if(current->left!=NULL)
+   {
+       lsmallest=minNode(current->left);
+   }
+   if(current->right!=NULL)
+   {
+       rsmallest=minNode(current->right);
+   }
+    if(lsmallest==NULL)
+    {
+        return rsmallest;
+    }
+    else if(rsmallest==NULL)
+    {
+        return lsmallest;
+    }
+    else
+    {
+        if(lsmallest->data<rsmallest->data)
+            return lsmallest;
+        else if(rsmallest->data<=lsmallest->data)
+            return rsmallest;
+    }
+    
+    
+}
+
+void delete(int deleteData,node current)
+{
+    if(current==NULL)
+    {
+        printf("\nDATA NOT FOUND...\n");
+    }
+    else if(current->data==deleteData)
+    {
+        if(current->left==NULL && current->right==NULL)
+        {
+            current=NULL;
+        }
+        else if(current->left!=NULL && current->right!=NULL)
+        {
+            node mNode=minNode(current);
+            int temp=mNode->data;
+            mNode->data=current->data;
+            current->data=temp;
+            delete(deleteData,current);
+        }
+        else
+        {
+            if(current->left==NULL)
+            {
+                current=current->right;
+            }
+            else
+            {
+                current=current->left;
+            }
+            
+        }
+        
+        printf("\nDATA DELETED\n");
+    }
+    else
+    {
+        if(deleteData<current->data)
+        {
+            delete(deleteData,current->left);
+        }
+        else
+        {
+            delete(deleteData,current->right);
+        }
+        
+    }
+}
+
 int main()
 {
     int i=0,d;
-    printf("ENTER \n1 TO CONTINUE \n 2 TO EXIT\n3 TO SEARCH:");
+    printf("ENTER \n1 TO CONTINUE \n 2 TO EXIT\n3 TO SEARCH\n4 TO DELETE:");
     scanf("%d",&i);
     while(i!=2){
        if(i==1){
@@ -86,11 +165,17 @@ int main()
        }
        else if(i==3)
        {
-           printf("ENTER DATA TO BE ENTERED:");
+           printf("ENTER DATA TO BE SEARCHED:");
             scanf("%d",&d);
             search(d,root);
        }
-         printf("ENTER \n1 TO CONTINUE \n 2 TO EXIT\n3 TO SEARCH:");
+       else if(i==4)
+       {
+           printf("ENTER DATA TO BE DELETED:");
+            scanf("%d",&d);
+            delete(d,root);
+       }
+         printf("ENTER \n1 TO CONTINUE \n 2 TO EXIT\n3 TO SEARCH\n4 TO DELETE:");
         scanf("%d",&i);
     }
     display(root);
